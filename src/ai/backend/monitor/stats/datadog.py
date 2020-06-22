cb8dbeaefa2e28cef7a51f460bc693caced847f5
Joongi Kim <joongi@lablup.com>
2020-06-22 16:10:35 +0900

    Update for the plugin API 2.0

--- src/ai/backend/monitor/stats/datadog.py
+++ src/ai/backend/monitor/stats/datadog.py
@@ -1,19 +1,37 @@
 import datadog
 
+from typing import (
+    Any,
+    Mapping,
+    Union,
+)
+
 from ai.backend.common.monitor import AbstractStatsMonitor
+from ai.backend.common.plugin.monitor import AbstractStatReporterPlugin
 
 
-class DatadogStatsMonitor(AbstractStatsMonitor):
+class DatadogStatsMonitor(AbstractStatReporterPlugin):
 
-    def __init__(self):
+    def __init__(self, plugin_config: Mapping[str, Any], local_config: Mapping[str, Any]) -> None:
+        super().__init__(plugin_config, local_config)
         self.statsd = None
 
-    def init(self, config):
-        datadog.initialize(api_key=config.datadog_api_key,
-                           app_key=config.datadog_app_key)
+    async def init(self) -> None:
+        datadog.initialize(api_key=self.plugin_config['datadog_api_key'],
+                           app_key=self.plugin_config['datadog_app_key'])
         self.statsd = datadog.statsd
+        self.statsd.__enter__()
 
-    def report_stats(self, report_type, metric, *args):
+    async def cleanup(self) -> None:
+        self.statsd.__exit__(None, None, None)
+
+    async def update_plugin_config(self, new_plugin_config: Mapping[str, Any]) -> None:
+        self.plugin_config = self.new_plugin_config
+        if self.statsd:
+            await self.statsd.cleanup()
+            await self.statsd.init()
+
+    async def report_stats(self, report_type: str, metric: Union[float, int], *args):
         if report_type == 'increment':
             self.statsd.increment(metric)
         elif report_type == 'gauge':
@@ -21,25 +39,3 @@ class DatadogStatsMonitor(AbstractStatsMonitor):
             self.statsd.gauge(metric, value)
         else:
             raise ValueError(f'Not supported report type: {report_type}')
-
-    def __enter__(self):
-        self.statsd.__enter__()
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.statsd.__exit__(type, value, traceback)
-
-
-def add_plugin_args(parser):
-    parser.add('--datadog-api-key', env_var='DATADOG_API_KEY',
-               type=str, default=None,
-               help='The API key for Datadog monitoring agent.')
-    parser.add('--datadog-app-key', env_var='DATADOG_APP_KEY',
-               type=str, default=None,
-               help='The application key for Datadog monitoring agent.')
-
-
-def get_plugin(config):
-    stats_monitor = DatadogStatsMonitor()
-    stats_monitor.init(config)
-    return stats_monitor
